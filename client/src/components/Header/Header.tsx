454554ba83694a7fdbcfb7cae314e1d01156ed0c
unknown <xocastillian@gmail.com>
2024-09-13 13:12:02 +0500

    deploe setts

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC<Props> = ({ onLogout }) => {
 	return (
 		<header className='flex items-center justify-between px-20 py-6 shadow-sm backdrop-blur-sm bg-stone-50'>
 			<h1>{userName}`s Notes</h1>
-			<LogOut onClick={onLogout} color='red' />
+			<LogOut onClick={onLogout} color='red' className='cursor-pointer' />
 		</header>
 	)
 }
