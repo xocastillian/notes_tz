ae85b58525736af502497a5d235b1be27cc4bb99
unknown <xocastillian@gmail.com>
2024-09-14 13:18:56 +0500

    redirect to auth

--- client/src/components/ProtectedRoutes.tsx
+++ client/src/components/ProtectedRoutes.tsx
@@ -1,6 +1,6 @@
 import { useAppSelector } from '@/store/hooks'
-import { LogIn } from 'lucide-react'
-import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 interface Props {
 	children: React.ReactNode
@@ -8,21 +8,15 @@ interface Props {
 
 const ProtectedRoutes: React.FC<Props> = ({ children }) => {
 	const isAuthorized = useAppSelector(state => state.user.isAuthorized)
+	const navigate = useNavigate()
 
-	return (
-		<>
-			{isAuthorized ? (
-				children
-			) : (
-				<div className='flex flex-col justify-center items-center pt-20 gap-10'>
-					<h1 className='text-2xl'>Unauthorized</h1>
-					<Link to={'/auth'}>
-						<LogIn />
-					</Link>
-				</div>
-			)}
-		</>
-	)
+	useEffect(() => {
+		if (!isAuthorized) {
+			navigate('/auth')
+		}
+	}, [isAuthorized, navigate])
+
+	return <>{isAuthorized && children}</>
 }
 
 export default ProtectedRoutes
